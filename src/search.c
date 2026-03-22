7d647ff1617a90bee8259c4be5fffc76d2b9caa5
Julius <julius.beyer@julbey.de>
2026-03-22 08:53:23 +0100

    more efficient search algorithm

--- src/search.c
+++ src/search.c
@@ -5,9 +5,70 @@
 #include "types.h"
 #include "ui.h"
 
-static void sortTop(Match *top, int top_n) {
-  for (int i = 0; i < top_n; i++) {
-    for (int j = i + 1; j < top_n; j++) {
+typedef struct {
+  Match *data;
+  int size;
+  int capacity;
+} MinHeap;
+
+static void heapify_down(MinHeap *heap, int idx) {
+  while (1) {
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < heap->size && heap->data[left].score < heap->data[smallest].score)
+      smallest = left;
+
+    if (right < heap->size && heap->data[right].score < heap->data[smallest].score)
+      smallest = right;
+
+    if (smallest == idx)
+      break;
+
+    Match tmp = heap->data[idx];
+    heap->data[idx] = heap->data[smallest];
+    heap->data[smallest] = tmp;
+
+    idx = smallest;
+  }
+}
+
+static void heapify_up(MinHeap *heap, int idx) {
+  while (idx > 0) {
+    int parent = (idx - 1) / 2;
+    if (heap->data[parent].score <= heap->data[idx].score)
+      break;
+
+    Match tmp = heap->data[idx];
+    heap->data[idx] = heap->data[parent];
+    heap->data[parent] = tmp;
+
+    idx = parent;
+  }
+}
+
+static int heap_insert(MinHeap *heap, Match match) {
+  if (heap->size >= heap->capacity)
+    return 0;
+
+  heap->data[heap->size] = match;
+  heapify_up(heap, heap->size);
+  heap->size++;
+  return 1;
+}
+
+static void heap_replace_min(MinHeap *heap, Match match) {
+  if (heap->size == 0)
+    return;
+
+  heap->data[0] = match;
+  heapify_down(heap, 0);
+}
+
+static void sortTopN(Match *top, int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
       if (top[j].score > top[i].score) {
         Match tmp = top[i];
         top[i] = top[j];
@@ -17,20 +78,14 @@ static void sortTop(Match *top, int top_n) {
   }
 }
 
-static void emptyTop(Match *top, int top_n) {
-  for (int i = 0; i < top_n; i++) {
-    top[i].name = NULL;
-    top[i].exec = NULL;
-    top[i].score = 0;
-  }
-}
-
 int search(App *app) {
   clearResUi(app->term.rows);
   if (!app->top)
     return 0;
 
-  emptyTop(app->top, app->app_count);
+  int max_rows = app->term.rows - 3;
+  if (max_rows < 0)
+    max_rows = 0;
 
   int result_count = 0;
 
@@ -42,19 +97,29 @@ int search(App *app) {
       result_count++;
     }
   } else {
+    MinHeap heap = {0};
+    heap.data = app->top;
+    heap.capacity = max_rows > 0 ? max_rows : 1;
+    heap.size = 0;
+
     for (int i = 0; i < app->app_count; i++) {
       int score = fuzzyScore(app->ui.query_lower, app->apps.nameLowerList[i],
                              app->ui.query_len, app->apps.nameLenList[i]);
 
       if (score > 0) {
-        app->top[result_count].name = app->apps.nameList[i];
-        app->top[result_count].exec = app->apps.execCmdList[i];
-        app->top[result_count].score = score;
-        result_count++;
+        Match match = {app->apps.nameList[i], app->apps.execCmdList[i], score};
+
+        if (heap.size < heap.capacity) {
+          heap_insert(&heap, match);
+          result_count++;
+        } else if (score > heap.data[0].score) {
+          heap_replace_min(&heap, match);
+        }
       }
     }
 
-    sortTop(app->top, result_count);
+    sortTopN(app->top, heap.size);
+    result_count = heap.size;
   }
 
   app->top_n = result_count;
